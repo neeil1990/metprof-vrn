6588e6463ca1815caa5d577493a36c863fe761e2
neeil1990 <neeil@mail.ru>
2019-08-30 11:35:22 +0300

    no message

--- sale/index.php
+++ sale/index.php
@@ -74,7 +74,7 @@ $APPLICATION->SetTitle("Акции");
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "Y",
 		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY1" => "DATE_ACTIVE_TO",
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
 		"SORT_ORDER2" => "ASC",
