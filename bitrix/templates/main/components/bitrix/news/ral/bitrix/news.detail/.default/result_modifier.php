dc0ab9ec3517e687a2d143235f341ff73ab08c86
neeil1990 <neeil@mail.ru>
2020-06-11 11:24:09 +0300

    ral

---
+++ bitrix/templates/main/components/bitrix/news/ral/bitrix/news.detail/.default/result_modifier.php
@@ -0,0 +1,35 @@
+<?php
+
+if($arResult['PROPERTIES']['CODE']['VALUE'] && is_numeric($arParams['CATALOG_IBLOCK_ID'])) {
+    $arSelect = Array(
+        "ID",
+        "IBLOCK_ID",
+        "NAME",
+        "DETAIL_PAGE_URL",
+        "PREVIEW_PICTURE",
+        "PROPERTY_CML2_BASE_UNIT"
+    );
+    $arFilter = Array("IBLOCK_ID" => $arParams['CATALOG_IBLOCK_ID'], "PROPERTY_TSVET_RAL_VALUE" => $arResult['PROPERTIES']['CODE']['VALUE']);
+    $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+    while ($ob = $res->GetNextElement()) {
+        $arFields = $ob->GetFields();
+        $arProps = $ob->GetProperties();
+
+        $arResult['ITEMS'][$arFields['ID']] = $arFields;
+        $arResult['ITEMS'][$arFields['ID']]['PREVIEW_PICTURE'] = CFile::GetPath($arFields['PREVIEW_PICTURE']);
+        $arResult['ITEMS'][$arFields['ID']]['PROPERTIES'] = $arProps;
+        $arResult['SECTIONS'][$arFields['IBLOCK_SECTION_ID']]['ID'] = $arFields['IBLOCK_SECTION_ID'];
+        $arResult['SECTIONS'][$arFields['IBLOCK_SECTION_ID']]['VALUE_XML_ID'] = $arProps['PROIZVODITEL']['VALUE_XML_ID'];
+    }
+
+    if(count($arResult['SECTIONS']) > 0){
+        foreach($arResult['SECTIONS'] as &$arSection){
+            $res = CIBlockSection::GetByID($arSection["ID"]);
+            if($ar_res = $res->GetNext()){
+                $ar_res['VALUE_XML_ID'] = $arSection['VALUE_XML_ID'];
+                $arSection = $ar_res;
+                $arSection['PICTURE'] = CFile::GetPath($arSection['PICTURE']);
+            }
+        }
+    }
+}
